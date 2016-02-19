3c17d145f4a8fe7ed0e1a1992d967214e84eedcb
Jasmine Green <jasgreen@cs.utexas.edu>
2016-02-18 18:18:44 -0600

    priority-sema working

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -61,19 +61,16 @@ void
 sema_down (struct semaphore *sema) 
 {
   enum intr_level old_level;
-//  char msg[100];
 
   ASSERT (sema != NULL);
   ASSERT (!intr_context ());
 
-  // snprintf(msg, 100, "sleep %s\n", thread_name());
-  // puts(msg);
-
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-      list_push_back (&sema->waiters, &thread_current ()->elem);
-      list_sort(&sema->waiters, &priority_check, NULL);
+      list_insert_ordered(&sema->waiters, &thread_current()->elem, &priority_check, NULL);
+      // list_push_back (&sema->waiters, &thread_current ()->elem);
+      // list_sort(&sema->waiters, &priority_check, NULL);
       thread_block ();
     }
   sema->value--;
@@ -119,21 +116,14 @@ sema_up (struct semaphore *sema)
   ASSERT (sema != NULL);
 
   old_level = intr_disable ();
-
+  sema->value++;
   if(!list_empty(&sema->waiters)) {
-    t = list_entry (list_front(&sema->waiters), struct thread, elem);
-
-    if(t->priority > thread_current()->priority) {
-      thread_unblock (list_entry (list_pop_front (&sema->waiters),
-                                  struct thread, elem));
-      thread_yield ();
-    }
-    else {
-      thread_unblock (list_entry (list_pop_front (&sema->waiters),
-                                  struct thread, elem));
+    t = list_entry(list_pop_front (&sema->waiters), struct thread, elem);
+    thread_unblock(t);
+    if(t->priority > thread_current()->priority && !intr_context()) {
+      thread_yield();
     }
   }
-  sema->value++;
   intr_set_level (old_level);
 }
 
--- src/threads/thread.c
+++ src/threads/thread.c
@@ -329,6 +329,7 @@ thread_yield (void)
   old_level = intr_disable ();
   if (cur != idle_thread) 
     list_push_back (&ready_list, &cur->elem);
+    //list_insert_ordered(&ready_list, &cur->elem, &priority_check, NULL);
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
